be80feac609d1b0a3a775d4bd45f08c87beae7d8
pratamaizzat <pratama.izzat231298@gmail.com>
2022-03-19 15:44:48 +0700

    add accessing route to root :tada: :sparkles:

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    statusText: 'OK',
+    message: 'Welcome To Simple Authentication System',
+  })
+})
+
+module.exports = app
---
+++ test/unit/welcomeMessage.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest')
+const app = require('../../src/app')
+
+describe('Return Welcome Message if user access root', () => {
+  test('should return welcome message with status code is 200', async () => {
+    const response = await request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200)
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        status: 200,
+        statusText: 'OK',
+        message: 'Welcome To Simple Authentication System',
+      })
+    )
+  })
+})
